0ca5d471db63996574cbaf654030a890ed1b9c84
Victor-Onyejide <59766880+Victor-Onyejide@users.noreply.github.com>
2021-07-07 17:48:57 -0400

    Update SalesByMatch.py

--- Warm-up Challenges/SalesByMatch.py
+++ Warm-up Challenges/SalesByMatch.py
@@ -6,35 +6,42 @@ import random
 import re
 import sys
 
-# Complete the sockMerchant function below.
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
 def sockMerchant(n, ar):
-    sum_up = {}
+    # Write your code here
+    colorsandpairs = {}
     for i in range(0,n-1):
         count = 1
-        # if sum_up:
-        if check_keys(sum_up.keys(), ar[i]) == True:
-                continue
-                
-        for j in range(i + 1,  n):
+        if check_keys(colorsandpairs.keys(), ar[i]) == True:
+            continue
+        for j in range(i+1,n):
             if ar[i] == ar[j]:
-                count += 1
-        pairs = math.floor(count / 2)
-        sum_up[ar[i]]= pairs
-    
-    #Total
-    total_sum = sum(sum_up.values())
+                count +=1
+        pairs = math.floor(count/2)
+        
+        colorsandpairs[ar[i]] = pairs
+        
+    total_sum = sum(colorsandpairs.values())
     return total_sum
-                
-def check_keys(sum_up_keys, l_i):
-    for k in sum_up_keys:
+    
+def check_keys(keys, l_i):
+    for k in keys:
         if k == l_i:
             return True
     return False
-    
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input())
+    n = int(input().strip())
 
     ar = list(map(int, input().rstrip().split()))
 
